9329fde7a247008066e1bdb86911469b52a9f695
Manticore16 <manticore01141987@live.com>
2026-07-20 21:13:49 -0400

    tests should pass

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -33,7 +33,7 @@ func TestGetAPIKey(t *testing.T) {
 		},
 		{
 			key:       "Authorization",
-			value:     "ApiKey x xxxxxx",
+			value:     "ApiKey xxxxxx",
 			expect:    "xxxxxx",
 			expectErr: "not expecting an error",
 		},
